ab9ecdddec7da052d84635bc5772d86cb966446f
ljl763461996 <1241318532@qq.com>
2025-04-08 19:49:12 +0800

    first push

---
+++ main/Config.py
@@ -0,0 +1,11 @@
+#coding:utf-8
+
+# 图片及视频检测结果保存路径
+save_path = 'save_data'
+
+# 使用的模型路径
+model_path = 'models/best.pt'
+
+names = {0: 'aeroplane',1: 'bicycle',2: 'bus',3: 'car',4: 'motorbike',5: 'person',6: 'train'}
+
+CH_names = ['飞机','自行车','公交车','小汽车','摩托车','行人','火车']
